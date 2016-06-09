10b65f1f1072af34863671c358482eeb75ef5a52
Raymond Blok <99034225@mydavinci.nl>
2016-06-09 12:12:00 +0200

    commit 3

--- application/model/InfoModel.php
+++ application/model/InfoModel.php
@@ -12,4 +12,24 @@ class InfoModel
 
 		return $query->fetchAll();
 	}
+
+	public static function updateProfileInfo($name, $surname, $occupation, $gender, $address, $email, $mobile, $intrest)
+	{
+		$database = DatabaseFactory::getFactory()->getConnection();
+
+		$sql = "UPDATE profiles 
+				SET name = :name, surname = :surname, occupation = :occupation, gender = :gender, address = :address, email = :email, mobile = :mobile, intrest = :intrest 
+				WHERE user_id = :user_id";
+		$query = $database ->prepare($sql);
+		$query = execute(array(':name' => $name, ':surname' => $surname, ':occupation' => $occupation, ':gender' => $gender, 
+				':address' => $address, ':email' => $email, ':mobile' => $mobile, ':intrest' => $intrest, 
+				':user_id' = Session::get('user_id')));
+
+        if ($query->rowCount() == 1) {
+            return true;
+        }
+
+        Session::add('feedback_negative', Text::get('FEEDBACK_NOTE_EDITING_FAILED'));
+        return false;
+	}
 }
---
+++ application/view/info/edit.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<h1>InfoController/edit/nobody cares</h1>
+
+	<div class="box">
+		<h2>edit your profile</h2>
+
+		<?php $this->renderFeedbackMessages(); ?>
+
+	</div>
+
+</div>
